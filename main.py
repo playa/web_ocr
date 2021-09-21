6afe5c1d872245c471a2a9886e06c2073cb96b5c
Yuri Barbashov <lolcoltd@gmail.com>
2021-09-21 12:44:30 +0300

    improvements for ocr

--- main.py
+++ main.py
@@ -25,16 +25,17 @@ def get_page_data(driver: Chrome, url: str) -> List[List[str]]:
     driver.get(url)
     sleep(1)
 
-    # increase text size
+    # increase text size, hide some data
     css = """
-        body {font-size: 1.5em !important;}
+        body {font-size: 2 em !important;}
         table#fix-columns-table td, table#fix-columns-table th {background: white !important; } 
         table#fix-columns-table td:nth-child(2) ~ * {color: white !important; text-align: center !important } 
-        table#fix-columns-table td > nobr { color: black !important; }
+        table#fix-columns-table td > nobr { color: black !important; padding: 2px;}
         table#fix-columns-table {height: auto;}
     """
     for style in css.split("\n")[1:-1]:
         driver.execute_script(f'document.styleSheets[0].insertRule("{style}", 0 )')
+    #driver.execute_script("document.body.style.zoom = '2'")
     sleep(1)
     results = []
     head_rows = driver.find_elements_by_css_selector('.table-borderless tr')
@@ -56,7 +57,8 @@ def get_page_data(driver: Chrome, url: str) -> List[List[str]]:
     driver.execute_script("document.getElementsByClassName('table-scroller')[0].removeAttribute('class')")
     driver.execute_script("document.getElementById('fix-columns-table').style.height= 'auto'")
     sleep(1)
-
+    #driver.save_screenshot("screen.png")
+    
     rows = driver.find_elements_by_css_selector('table#fix-columns-table tr')
     for i, row in enumerate(rows):
 
@@ -70,9 +72,13 @@ def get_page_data(driver: Chrome, url: str) -> List[List[str]]:
                 name = name.split('. ', 1)[1]
             fullrow = [name]
             for j in range(2, len(cols)):
-                image = Image.open(BytesIO(b64decode(cols[j].screenshot_as_base64)))
+                #image = Image.open(BytesIO(b64decode(cols[j].screenshot_as_base64)))
+                #image.save(f"{i}_{j}_col.png")
+                nobr = cols[j].find_elements_by_tag_name('nobr')[0]
+                image = Image.open(BytesIO(b64decode(nobr.screenshot_as_base64)))
                 #image.save(f"{i}_{j}.png")
-                text = pytesseract.image_to_string(image, config='--psm 6 -c "tessedit_char_whitelist=0123456789"')
+                # 7 - single line option, 8 is single word 
+                text = pytesseract.image_to_string(image, config='--psm 8 -c "tessedit_char_whitelist=0123456789"')
                 fullrow.append(text.strip())
             results.append(fullrow) 
     return results
